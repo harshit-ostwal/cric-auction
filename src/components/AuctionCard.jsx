f03e5f0200ae2e8fa6c78f6348f49bf0d6094cd8
Harshit Jain <harshitostwal1234@gmail.com>
2025-09-27 18:39:03 +0530

    feat: Refactor AuctionCard layout for improved structure and responsiveness

--- src/components/AuctionCard.jsx
+++ src/components/AuctionCard.jsx
@@ -119,157 +119,158 @@ const AuctionCard = memo(({ auction, currentUser }) => {
     };
 
     return (
-        <Card className={"bg-radial from-white via-white to-amber-50"}>
-            <CardHeader className="flex flex-col gap-4">
-                <div className="flex w-full items-center justify-between gap-4 font-semibold">
-                    <Heading size="p">
-                        <Icons.calendar />
-                        {auction.auctionDate ? formattedDate : "Date not set"}
-                    </Heading>
-                    <Heading size="p">
-                        <Icons.clock />
-                        {auction.auctionTime ? formattedTime : "Time not set"}
-                    </Heading>
-                </div>
+        <div className="flex w-full flex-col gap-4">
+            <Card className={"bg-radial from-white via-white to-amber-50"}>
+                <CardHeader className="flex flex-col gap-4">
+                    <div className="flex w-full items-center justify-between gap-4 font-semibold">
+                        <Heading size="p">
+                            <Icons.calendar />
+                            {auction.auctionDate
+                                ? formattedDate
+                                : "Date not set"}
+                        </Heading>
+                        <Heading size="p">
+                            <Icons.clock />
+                            {auction.auctionTime
+                                ? formattedTime
+                                : "Time not set"}
+                        </Heading>
+                    </div>
 
-                <div
-                    className="flex w-fit cursor-pointer items-center gap-4"
-                    onClick={() => router.replace(`/auctions/${auction.id}`)}
-                >
-                    <Avatar
-                        className={
-                            "size-16 rounded-2xl object-cover lg:size-24"
+                    <div
+                        className="flex w-fit cursor-pointer items-center gap-4"
+                        onClick={() =>
+                            router.replace(`/auctions/${auction.id}`)
                         }
                     >
-                        {auction.auctionLogo ? (
-                            <AvatarImage
-                                src={auction.auctionLogo}
-                                alt={auction.auctionName}
-                            />
-                        ) : (
-                            <AvatarFallback
-                                className={"rounded-2xl object-cover"}
-                            >
-                                {getAuctionInitials(auction.auctionName)}
-                            </AvatarFallback>
-                        )}
-                    </Avatar>
-                    <div className="flex flex-col">
-                        <CardTitle>
-                            <Heading
-                                size="h5"
-                                className="line-clamp-1 font-semibold text-ellipsis"
-                            >
-                                {auction.auctionName}
-                            </Heading>
-                        </CardTitle>
-                        <CardDescription
-                            className={"text-foreground flex flex-col gap-1"}
+                        <Avatar
+                            className={
+                                "size-16 rounded-2xl object-cover lg:size-24"
+                            }
                         >
-                            <Heading
-                                size="p"
-                                className="text-muted-foreground line-clamp-1 font-semibold text-ellipsis"
-                            >
-                                {auction.venue || "Venue not set"}
-                            </Heading>
-                        </CardDescription>
-                    </div>
-                </div>
-            </CardHeader>
-
-            <CardContent className={"flex items-center justify-between gap-6"}>
-                <div className="flex flex-col gap-1">
-                    <Heading size="p" className="text-muted-foreground">
-                        Team Points (Per Team)
-                    </Heading>
-                    <Heading size="p" className="font-semibold">
-                        <Icons.targetArrow className="text-red-600" />
-                        {auction.teamPoints?.toLocaleString() || 0}
-                    </Heading>
-                </div>
-
-                <div className="flex flex-col gap-1">
-                    <Heading size="p" className="text-muted-foreground">
-                        Total Players (Per Team)
-                    </Heading>
-                    <Heading size="p" className="font-semibold">
-                        <Icons.target className="text-purple-600" />
-                        {auction.playerPerTeam || 0}
-                    </Heading>
-                </div>
-            </CardContent>
-
-            {isOwner && (
-                <CardContent className="pt-0">
-                    <div className="flex w-full gap-2">
-                        <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                                <Button variant="outline" className="flex-1">
-                                    <Icons.edit />
-                                    Edit
-                                </Button>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle>
-                                        Are you sure you want to edit this
-                                        auction?
-                                    </AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        This action will allow you to edit the
-                                        auction details.
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                    <AlertDialogCancel>
-                                        Cancel
-                                    </AlertDialogCancel>
-                                    <AlertDialogAction
-                                        onClick={() => handleEdit(auction.id)}
-                                    >
-                                        Edit Auction
-                                    </AlertDialogAction>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
-                        <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                                <Button
-                                    variant="destructive"
-                                    className="flex-1"
+                            {auction.auctionLogo ? (
+                                <AvatarImage
+                                    src={auction.auctionLogo}
+                                    alt={auction.auctionName}
+                                />
+                            ) : (
+                                <AvatarFallback
+                                    className={"rounded-2xl object-cover"}
                                 >
-                                    <Icons.delete />
-                                    Delete
-                                </Button>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle>
-                                        Are you sure you want to delete this
-                                        auction?
-                                    </AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        This action cannot be undone. This will
-                                        permanently delete the auction and all
-                                        associated data.
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                    <AlertDialogCancel>
-                                        Cancel
-                                    </AlertDialogCancel>
-                                    <AlertDialogAction
-                                        onClick={() => handleDelete(auction.id)}
-                                    >
-                                        Delete Auction
-                                    </AlertDialogAction>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
+                                    {getAuctionInitials(auction.auctionName)}
+                                </AvatarFallback>
+                            )}
+                        </Avatar>
+                        <div className="flex flex-col">
+                            <CardTitle>
+                                <Heading
+                                    size="h5"
+                                    className="line-clamp-1 font-semibold text-ellipsis"
+                                >
+                                    {auction.auctionName}
+                                </Heading>
+                            </CardTitle>
+                            <CardDescription
+                                className={
+                                    "text-foreground flex flex-col gap-1"
+                                }
+                            >
+                                <Heading
+                                    size="p"
+                                    className="text-muted-foreground line-clamp-1 font-semibold text-ellipsis"
+                                >
+                                    {auction.venue || "Venue not set"}
+                                </Heading>
+                            </CardDescription>
+                        </div>
+                    </div>
+                </CardHeader>
+
+                <CardContent
+                    className={"flex items-center justify-between gap-6"}
+                >
+                    <div className="flex flex-col gap-1">
+                        <Heading size="p" className="text-muted-foreground">
+                            Team Points (Per Team)
+                        </Heading>
+                        <Heading size="p" className="font-semibold">
+                            <Icons.targetArrow className="text-red-600" />
+                            {auction.teamPoints?.toLocaleString() || 0}
+                        </Heading>
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                        <Heading size="p" className="text-muted-foreground">
+                            Total Players (Per Team)
+                        </Heading>
+                        <Heading size="p" className="font-semibold">
+                            <Icons.target className="text-purple-600" />
+                            {auction.playerPerTeam || 0}
+                        </Heading>
                     </div>
                 </CardContent>
+            </Card>
+            {isOwner && (
+                <div className="flex w-full gap-2">
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="outline" className="flex-1">
+                                <Icons.edit />
+                                Edit
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    Are you sure you want to edit this auction?
+                                </AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action will allow you to edit the
+                                    auction details.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    onClick={() => handleEdit(auction.id)}
+                                >
+                                    Edit Auction
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="destructive" className="flex-1">
+                                <Icons.delete />
+                                Delete
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    Are you sure you want to delete this
+                                    auction?
+                                </AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will
+                                    permanently delete the auction and all
+                                    associated data.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    onClick={() => handleDelete(auction.id)}
+                                >
+                                    Delete Auction
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
             )}
-        </Card>
+        </div>
     );
 });
 AuctionCard.displayName = "AuctionCard";
